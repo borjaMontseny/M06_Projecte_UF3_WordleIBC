53b494c92c924ae6089fbab6e34205b06c76e352
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2024-02-18 21:33:21 +0100

    Commit formulari
    Falten coses

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 // borjaMontseny & AdriaChillon DAW2 M06 2024
+import dic from './diccionari.js'; // Importem la variable del fitxer diccionari.js
+
+// Funció per a comprobar la RegExp
+function comprobarRegExp(regex, text, checkEx) {
+    if (regex.test(text)) {
+        checkEx.classList.remove('ocult');
+        checkEx.classList.add('visible');
+    } else {
+        checkEx.classList.remove('visible');
+        checkEx.classList.add('ocult');
+    }
+}
+
+// Expresions Regulars
+var regExpBasica = /^\w+$/;
+var regExpCorreuElectronic = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+var regExpTelegon = /^(?:[9876]\d{8}|[9876]\d{2} \d{3} \d{3}|[9876]\d{1} \d{3} \d{2} \d{2})$/;
 
 window.onload = function () {
 
-    // Jugador que es crea al formulari
+    /* 
+        1ª PART: FORMULARI 
+        - Validem el formulari (utilitzant regExp) i creem l'objecte jugador
+        en cas de que no hi hagin dades prèvies a LocalStorage (anterior partida)
+    */
+
+
+    // Definim la clase Jugador que es creará a partir del formulari
     class Jugador {
         constructor(nom, cognom, correuElectronic, telefon) {
             this.nom = nom;
@@ -11,18 +35,30 @@ window.onload = function () {
             this.telefon = telefon;
         }
 
-        // falta un constructor amb les dades de la partida, estadistiques etc. sino per defecte serien 0 al constructor de adalt
+        // falta afegir dades de la partida, estadistiques etc
     }
 
     document.getElementById('formulariJoc').addEventListener('submit', function (event) {
         event.preventDefault();
 
+        // Capturem els elements HTML del formulari, input i check (emoji ✔️)
         var nom = document.getElementById('nom').value;
-        var cognom = document.getElementById('cognom').value;
-        var correuElectronic = document.getElementById('correuElectronic').value;
-        var telefon = document.getElementById('telefon').value;
+        var checkNom = documentdocument.getElementById('checkNom');
 
-        const jugador = new Jugador(nom, cognom, correuElectronic, telefon);
+        var cognom = document.getElementById('cognom').value;
+        var checkNom = documentdocument.getElementById('checkCogom');
+
+        var correuElectronic = document.getElementById('correuElectronic').value;
+        var checkNom = documentdocument.getElementById('checkCorreuElectronic');
+
+        var telefon = document.getElementById('telefon').value;
+        var checkNom = documentdocument.getElementById('checkCorreuElectronic');
+
+        // Validem els camps
+
+
+        // Instanciem l'objecte jugador
+        var jugador = new Jugador(nom, cognom, correuElectronic, telefon);
 
         // guardem les dades del jugador en el LocalStorage
         localStorage.setItem('jugador', JSON.stringify(jugador));
