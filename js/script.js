ca69c6684f91500096bf193aab0615f26d2fb8cd
Borja Montseny <borja.montseny@gmail.com>
2024-02-19 13:40:16 +0100

    Commit Formulari
    Validació de nom

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 // borjaMontseny & AdriaChillon DAW2 M06 2024
 import dic from './diccionari.js'; // Importem la variable del fitxer diccionari.js
 
+// Definim la clase Jugador que es creará a partir del formulari
+// TODO: Comprobar si a LocalStorage ja hi existeix un jugador
+class Jugador {
+    // Constructor per si es el primer cop que juguem:
+    constructor(nom, cognom, correuElectronic, telefon) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.correuElectronic = correuElectronic;
+        this.telefon = telefon;
+    }
+
+    // falta afegir dades de la partida, estadistiques etc
+}
+
 // Funció per a comprobar la RegExp
 function comprobarRegExp(regex, text, checkEx) {
+    console.log(regex, text, checkEx, regex.test(text));
     if (regex.test(text)) {
         checkEx.classList.remove('ocult');
         checkEx.classList.add('visible');
@@ -13,10 +28,11 @@ function comprobarRegExp(regex, text, checkEx) {
 }
 
 // Expresions Regulars
-var regExpBasica = /^\w+$/;
+var regExpBasica = /^\w+$/; // Per a nom i cognom
 var regExpCorreuElectronic = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 var regExpTelegon = /^(?:[9876]\d{8}|[9876]\d{2} \d{3} \d{3}|[9876]\d{1} \d{3} \d{2} \d{2})$/;
 
+// Carrega la web
 window.onload = function () {
 
     /* 
@@ -25,37 +41,30 @@ window.onload = function () {
         en cas de que no hi hagin dades prèvies a LocalStorage (anterior partida)
     */
 
+    // Capturem els elements HTML del formulari, input i check (emoji ✔️)
+    var nom = document.getElementById('nom');
+    var valorNom = nom.value;
+    var checkNom = document.getElementById('checkNom');
 
-    // Definim la clase Jugador que es creará a partir del formulari
-    class Jugador {
-        constructor(nom, cognom, correuElectronic, telefon) {
-            this.nom = nom;
-            this.cognom = cognom;
-            this.correuElectronic = correuElectronic;
-            this.telefon = telefon;
-        }
+    var cognom = document.getElementById('cognom');
+    var valorCognom = document.getElementById('cognom').value;
+    var checkCognom = document.getElementById('checkCogom');
 
-        // falta afegir dades de la partida, estadistiques etc
-    }
+    var correuElectronic = document.getElementById('correuElectronic').value;
+    var checkCorreuElectronic = document.getElementById('checkCorreuElectronic');
 
+    var telefon = document.getElementById('telefon').value;
+    var checkTelefon = document.getElementById('checkCorreuElectronic');
+
+    // Validem els camps
+    // Nom:
+    nom.addEventListener("input", function () {
+        comprobarRegExp(regExpBasica, nom.value, checkNom);
+    });
+
+    // se validan los campos, se instancia jugador, se guarda en localStorage, y desaparece el formulario
     document.getElementById('formulariJoc').addEventListener('submit', function (event) {
         event.preventDefault();
-
-        // Capturem els elements HTML del formulari, input i check (emoji ✔️)
-        var nom = document.getElementById('nom').value;
-        var checkNom = documentdocument.getElementById('checkNom');
-
-        var cognom = document.getElementById('cognom').value;
-        var checkNom = documentdocument.getElementById('checkCogom');
-
-        var correuElectronic = document.getElementById('correuElectronic').value;
-        var checkNom = documentdocument.getElementById('checkCorreuElectronic');
-
-        var telefon = document.getElementById('telefon').value;
-        var checkNom = documentdocument.getElementById('checkCorreuElectronic');
-
-        // Validem els camps
-
 
         // Instanciem l'objecte jugador
         var jugador = new Jugador(nom, cognom, correuElectronic, telefon);
