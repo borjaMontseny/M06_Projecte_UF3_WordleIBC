b42c2f9702b88d15375d4d774bb1fffad3e237c9
AdriaChillon <144825302+AdriaChillon@users.noreply.github.com>
2024-02-28 19:33:49 +0100

    Funcionalidades básicas hechas

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ class Jugador {
     }
 }
 
+var modal =  new bootstrap.Modal(document.getElementById("modalForm"));
+
+function mostrarForm() {
+   modal.show();
+}
+
 function mostrarOAmagarElement(accio, element) {
     switch (accio) {
         case "amagar":
@@ -43,114 +49,123 @@ var regExpBasica = /^\w+$/;
 var regExpCorreuElectronic = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 var regExpTelefon = /^(?:[9876]\d{8}|[9876]\d{2} \d{3} \d{3}|[9876]\d{1} \d{3} \d{2} \d{2})$/;
 
-window.onload = function () {
-    var joc = document.getElementById('joc');
-    var panellSuperiorButtons = document.querySelectorAll(".panellSuperiorButton");
-    var formulari = document.getElementById('formulari');
-    var nom = document.getElementById('nom');
-    var checkNom = document.getElementById('checkNom');
-    var cognom = document.getElementById('cognom');
-    var checkCognom = document.getElementById('checkCognom');
-    var correuElectronic = document.getElementById('correuElectronic');
-    var checkCorreuElectronic = document.getElementById('checkCorreuElectronic');
-    var telefon = document.getElementById('telefon');
-    var checkTelefon = document.getElementById('checkTelefon');
+var joc = document.getElementById('joc');
+var panellSuperiorButtons = document.querySelectorAll(".panellSuperiorButton");
+var nom = document.getElementById('nom');
+var checkNom = document.getElementById('checkNom');
+var cognom = document.getElementById('cognom');
+var checkCognom = document.getElementById('checkCognom');
+var correuElectronic = document.getElementById('correuElectronic');
+var checkCorreuElectronic = document.getElementById('checkCorreuElectronic');
+var telefon = document.getElementById('telefon');
+var checkTelefon = document.getElementById('checkTelefon');
+var jugador = new Jugador(nom.value, cognom.value, correuElectronic.value, telefon.value, 0, 0, 0, 0);
 
-    // FORMULARI
-    nom.addEventListener("input", function () {
-        isNomValid = comprobarRegExp(regExpBasica, nom.value);
-        if (isNomValid) {
-            mostrarOAmagarElement("mostrar", checkNom);
-        }
-    });
-
-    cognom.addEventListener("input", function () {
-        isCognomValid = comprobarRegExp(regExpBasica, cognom.value);
-        if (isCognomValid) {
-            mostrarOAmagarElement("mostrar", checkCognom);
-        }
-    });
-
-    correuElectronic.addEventListener("input", function () {
-        isCorreuElectronicValid = comprobarRegExp(regExpCorreuElectronic, correuElectronic.value);
-        if (isCorreuElectronicValid) {
-            mostrarOAmagarElement("mostrar", checkCorreuElectronic);
-        }
-    });
-
-    telefon.addEventListener("input", function () {
-        isTelefonValid = comprobarRegExp(regExpTelefon, telefon.value);
-        if (isTelefonValid) {
-            mostrarOAmagarElement("mostrar", checkTelefon);
-        }
-    })
-
-    document.getElementById('gameStartButton').addEventListener('click', function (event) {
-        event.preventDefault();
-
-        if (isNomValid && isCognomValid && isCorreuElectronicValid && isTelefonValid) {
-            // Un cop el forumulari está validat, l'amaguem
-            mostrarOAmagarElement("amagar", formulari);
-
-            // Instanciem jugador amb les dades ja validades, i el guardem a localStorage
-            var jugador = new Jugador(nom.value, cognom.value, correuElectronic.value, telefon.value, 0, 0, 0, 0);
-            localStorage.setItem('jugador', JSON.stringify(jugador));
-            mostrarOAmagarElement("amagar", formulari);
-            mostrarOAmagarElement("mostrar", joc);
-        } else {
-            // Utiliza SweetAlert2 para mostrar el mensaje de error
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Valida tots els camps per a començar la partida',
-            });
-        }
-    });
-
-    // JOC:
-    // Panell Superior
-    panellSuperiorButtons.forEach(function (button) {
-        button.addEventListener("click", function (event) {
-            // Obtener el emoji clickeado
-            // var botoClicat = button.textContent;
-            var emojiClicked = button.textContent;
-
-            // Ejecutar una función o acción basada en el emoji clickeado
-            switch (emojiClicked) {
-                case "📊":
-                    // Estadístiques
-                    Swal.fire("Estadístiques del Jugador", `
-                    Nom: ${jugador.nom}
-                    Cognom: ${jugador.cognom}
-                    Correu Electrònic: ${jugador.correuElectronic}
-                    Telèfon: ${jugador.telefon}
-                    Partides Realitzades: ${jugador.partidesRealitzades}
-                    Partides Guanyades: ${jugador.partidesGuanyades}
-                    Millor Partida: ${jugador.millorPartida}
-                    Partida Més Ràpida: ${jugador.partidaMesRapida}
-                `);
-                    break;
-                case "🔄":
-                    // Començar nova partida
-                    Swal.fire({
-                        icon: "question",
-                        title: "Tornar a començar",
-                        text: "Estàs apunt de reiniciar la partida, estàs segur?",
-                        footer: '<a href="#">Desitjo reiniciar partida</a>'
-                    });
-                    break;
-                case "❔":
-                    // Guía de com jugar
-                    Swal.fire({
-                        title: "Com es juga?",
-                        text: "Implementar guia",
-                        icon: "question"
-                    });
-                    break;
-                default:
-                    break;
-            }
-        });
-    });
-
+// Verificar si ya hay datos en localStorage para la clave "jugador"
+if (!localStorage.getItem("jugador")) {
+    // Si no hay datos guardados, mostrar el formulario
+    mostrarForm();
+} else {
+    // Si ya hay datos guardados, mostrar el juego directamente
+    joc.classList.remove('ocult');
+    joc.classList.add('visible');
 }
+
+
+// FORMULARI
+nom.addEventListener("input", function () {
+    isNomValid = comprobarRegExp(regExpBasica, nom.value);
+    if (isNomValid) {
+        mostrarOAmagarElement("mostrar", checkNom);
+    } else {
+        joc.classList.remove('ocult');
+        joc.classList.add('visible');
+    }
+});
+
+cognom.addEventListener("input", function () {
+    isCognomValid = comprobarRegExp(regExpBasica, cognom.value);
+    if (isCognomValid) {
+        mostrarOAmagarElement("mostrar", checkCognom);
+    }
+});
+
+correuElectronic.addEventListener("input", function () {
+    isCorreuElectronicValid = comprobarRegExp(regExpCorreuElectronic, correuElectronic.value);
+    if (isCorreuElectronicValid) {
+        mostrarOAmagarElement("mostrar", checkCorreuElectronic);
+    }
+});
+
+telefon.addEventListener("input", function () {
+    isTelefonValid = comprobarRegExp(regExpTelefon, telefon.value);
+    if (isTelefonValid) {
+        mostrarOAmagarElement("mostrar", checkTelefon);
+    }
+})
+
+document.getElementById('gameStartButton').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (isNomValid && isCognomValid && isCorreuElectronicValid && isTelefonValid) {
+        // Instanciem jugador amb les dades ja validades, i el guardem a localStorage
+        jugador = new Jugador(nom.value, cognom.value, correuElectronic.value, telefon.value, 0, 0, 0, 0);
+        localStorage.setItem('jugador', JSON.stringify(jugador));
+        modal.hide();
+        joc.classList.remove('ocult');
+        joc.classList.add('visible');
+    } else {
+        // Utiliza SweetAlert2 para mostrar el mensaje de error
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Valida tots els camps per a començar la partida',
+        });
+    }
+});
+
+// JOC:
+// Panell Superior
+panellSuperiorButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        // Obtener el emoji clickeado
+        var emojiClicked = button.textContent;
+
+        // Ejecutar una función o acción basada en el emoji clickeado
+        switch (emojiClicked) {
+            case "📊":
+                // Estadístiques
+                Swal.fire({
+                    title: "Estadístiques",
+                    html: "Nom: " + jugador.nom + "<br>" +
+                          "Cognom: " + jugador.cognom + "<br>" +
+                          "Correu Electrònic: " + jugador.correuElectronic + "<br>" +
+                          "Telèfon: " + jugador.telefon + "<br>" +
+                          "Partides Realitzades: " + jugador.partidesRealitzades + "<br>" +
+                          "Partides Guanyades: " + jugador.partidesGuanyades + "<br>" +
+                          "Millor Partida: " + jugador.millorPartida + "<br>" +
+                          "Partida Més Ràpida: " + jugador.partidaMesRapida + "<br>"
+                });
+                
+                break;
+            case "🔄":
+                //reinciarPartida();
+                break;
+            case "❔":
+                // Guía de com jugar
+                Swal.fire({
+                    title: "Com jugar al WordleIBC?",
+                    html: "<p>Endevina el wordle <b>WORDLE</b> en 6 intents. <br><br> Has d'introduir paraules de 5 lletres <u>que \
+                    existeixin</u> i fer clic a ENTER (↵).<br><br> Després de cada intent, el color de les lletres canviarà per indicar l'evolució de la partida. <br><br>\
+                    No es tenen en compte els accents a l'hora d'introduir paraules. <br><br>Es poden repetir lletres. <br><br> Exemples:<br><br> \
+                    <h6><span id='p'>P</span>ILOT</h6> <p>La lletra <b>P</b> es troba en lloc correcte de la paraula.</p> \
+                    <h6>DO<span id='t'>T</span>ZE</h6> <p>La paraula té la lletra <b>T</b> però en un altre lloc.</p> \
+                    <h6>MAGI<span id='c'>C</span></h6> <p>La paraula no conté la lletra <b>C</b>.</p>",
+                    icon: "info"
+                });
+                break;
+            default:
+                break;
+        }
+    });
+});
